ce335844e95250caabf2692c668bcb1729f76894
KennethFajardo <johnkennethfajardo@gmail.com>
2021-03-16 18:13:51 +0800

    Create the functions makeCacheMatrix and cacheSolve which are used to check and store previously used matrices

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a cachable matrix by creating getters and setters
+## which foreshadows object-oriented programming
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL               #initialize m
+    set <- function(y) {    #set the function for initializing local variables
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x     #get the function for initializing local variables
+    setInv <- function(mat) m <<- solve(mat) #set the function for solving for the inverse
+    getInv <- function() m  #get the function for solving for the inverse
+    list(set = set, get = get, #return the functions made in makeCacheMatrix
+         setInv = setInv,
+         getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getInv()
+    # If the matrix is cached
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    # Else cache the new matrix and its inverse
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInv(m) #
+    m
 }
